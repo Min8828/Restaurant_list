7c83bf10e87f95897eecffcb52aa955d08b61213
Min8828 <minrou9928@gmail.com>
2023-04-11 20:56:21 +0800

    feat: modified search function

--- app.js
+++ app.js
@@ -104,18 +104,24 @@ app.post("/restaurants/:_id/delete", async (req, res) => {
   }
 });
 
-app.get("/search", (req, res) => {
+app.get("/search", async (req, res) => {
   if (!req.query.keyword) return res.redirect("/");
 
   // search by name or category of restaurant
   const keyword = req.query.keyword.trim().toLowerCase();
-  const filteredRestaurants = restaurants.filter(
-    (restaurant) =>
-      restaurant.name.toLowerCase().includes(keyword) ||
-      restaurant.category.toLowerCase().includes(keyword)
-  );
 
-  res.render("index", { restaurants: filteredRestaurants, keyword });
+  try {
+    const restaurants = await Restaurant.find({}).lean().exec();
+    const filteredRestaurants = restaurants.filter(
+      (restaurant) =>
+        restaurant.name.toLowerCase().includes(keyword) ||
+        restaurant.category.toLowerCase().includes(keyword)
+    );
+
+    res.render("index", { restaurants: filteredRestaurants, keyword });
+  } catch {
+    (err) => console.log(err);
+  }
 });
 
 // start and listen on the Express server
